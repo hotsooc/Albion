c8b1243d3a3c3b03514b19170a7efb8f21b423df
Hieu Nguyen <hieund@ngs.com.vn>
2025-09-03 16:16:50 +0700

    video

--- src/store/video.ts
+++ src/store/video.ts
@@ -14,7 +14,7 @@ export const VideoAlbion: VideoType[] = [
     // },
     {
         name: 'Chết ngu part 1',
-        url: '/video/Albion/Chet_ngu_part1.mp4',
+        url: '/video/Albion/Chet_ngu_part_1.mp4',
         thumbnail: 'URL_HINH_ANH_THU_NHO_1.jpg',
         description: 'Nor chết ngu'
     },
@@ -24,16 +24,16 @@ export const VideoAlbion: VideoType[] = [
         thumbnail: 'URL_HINH_ANH_THU_NHO_2.jpg',
         description: 'Phần tiếp theo của chuỗi video về những khoảnh khắc đáng nhớ.'
     },
-    // {
-    //     name: 'Chết ngu part 3',
-    //     url: '/video/Albion/Chet_ngu_part3.mp4',
-    //     thumbnail: 'URL_HINH_ANH_THU_NHO_3.jpg',
-    //     description: 'Phần cuối cùng trong chuỗi video với những pha xử lý bất ngờ.'
-    // },
     {
         name: 'Chết ngu part 3',
-        url: '/video/Albion/Chet_ngu_part_31.mp4',
+        url: 'https://res.cloudinary.com/dkgc5jg4s/video/upload/v1756889799/Chet_ngu_part_3_pkmjrc.mp4',
         thumbnail: 'URL_HINH_ANH_THU_NHO_3.jpg',
         description: 'Phần cuối cùng trong chuỗi video với những pha xử lý bất ngờ.'
-    }
+    },
+    // {
+    //     name: 'Highlight',
+    //     url: 'https://www.youtube.com/watch?v=dAi2Bl-kStM&feature=youtu.be',
+    //     thumbnail: 'URL_HINH_ANH_THU_NHO_3.jpg',
+    //     description: 'Phần cuối cùng trong chuỗi video với những pha xử lý bất ngờ.'
+    // }
 ];
