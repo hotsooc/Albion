961ce8ef57b1a6f96a1a02d0522db1ba49c62857
Hieu Nguyen <hieund@ngs.com.vn>
2025-09-03 15:31:27 +0700

    fix video

--- src/store/video.ts
+++ src/store/video.ts
@@ -6,9 +6,15 @@ export type VideoType = {
 }
 
 export const VideoAlbion: VideoType[] = [
+    // {
+    //     name: 'Chết ngu part 1',
+    //     url: '/video/Albion/Chet_ngu.mp4',
+    //     thumbnail: 'URL_HINH_ANH_THU_NHO_1.jpg',
+    //     description: 'Nor chết ngu'
+    // },
     {
         name: 'Chết ngu part 1',
-        url: '/video/Albion/Chet_ngu.mp4',
+        url: '/video/Albion/Chet_ngu_part1.mp4',
         thumbnail: 'URL_HINH_ANH_THU_NHO_1.jpg',
         description: 'Nor chết ngu'
     },
@@ -18,9 +24,15 @@ export const VideoAlbion: VideoType[] = [
         thumbnail: 'URL_HINH_ANH_THU_NHO_2.jpg',
         description: 'Phần tiếp theo của chuỗi video về những khoảnh khắc đáng nhớ.'
     },
+    // {
+    //     name: 'Chết ngu part 3',
+    //     url: '/video/Albion/Chet_ngu_part3.mp4',
+    //     thumbnail: 'URL_HINH_ANH_THU_NHO_3.jpg',
+    //     description: 'Phần cuối cùng trong chuỗi video với những pha xử lý bất ngờ.'
+    // },
     {
         name: 'Chết ngu part 3',
-        url: '/video/Albion/Chet_ngu_part3.mp4',
+        url: '/video/Albion/Chet_ngu_part_3.mp4',
         thumbnail: 'URL_HINH_ANH_THU_NHO_3.jpg',
         description: 'Phần cuối cùng trong chuỗi video với những pha xử lý bất ngờ.'
     }
